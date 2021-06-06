41a92427dd3f63c54e307c893b57d227ae400fab
jeyofdev <jgregoire.pro@gmail.com>
2021-06-06 10:53:12 +0200

    feat : add the configuration to the database

--- routes/members.js
+++ routes/members.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../config/connection');
 
 router.get('/', (req, res) => {
     res.status(200).send('list of members');
