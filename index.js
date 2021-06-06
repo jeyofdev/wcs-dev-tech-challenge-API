323eaf9c6a6b3565117a65189b977e64f0c70fa8
jeyofdev <jgregoire.pro@gmail.com>
2021-06-06 10:36:57 +0200

    feat : init the API

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to the API');
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log(`Server is running on ${port}`);
+});
